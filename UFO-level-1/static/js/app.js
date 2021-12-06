12c7e439289a4d0ce764337eb6950fbb7a37cf83
Margot <margoterudy@gmail.com>
2021-12-06 13:18:02 -0600

    margot html

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -7,8 +7,21 @@ console.log(tableData);
 // Create References
 var tbody = d3.select("tbody");
 
-tbody.append("tr").append("td").text("Hello!");
+// TEST 
+// tbody.append("tr").append("td").text("Hello!");
 
+//FORMAT AND BUTTON
+var $tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
 var inputFieldDate = d3.select("#datetime");
 var inputFieldCity = d3.select("#city");
 var columns = ["datetime", "city", "state", "country", "shape", "comments"]
+
+//Insert Data into HTML
+var addData = (dataInput) => {
+    dataInput.forEach(ufoSightings => {
+        var row = $tbody.append("tr");
+        columns.forEach(column => row.append("td").text(ufoSightings[column])
+        )
+    });
+}
