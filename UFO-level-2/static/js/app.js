181e7b6f560ceb211a448a366c85129a53170f4a
Margot <margoterudy@gmail.com>
2021-12-10 11:57:32 -0600

    state

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -4,11 +4,17 @@ var dataTable = data;
 // TEST 
 // tbody.append("tr").append("td").text("Hello!");
 
-// //FORMAT AND FILTER BUTTON (code from level 1)
+// //FORMAT AND FILTER BUTTONS plus addl buttons (code from level 1)
 var $tbody = d3.select("tbody");
 var button = d3.select("#filter-btn");
 var inputFieldDate = d3.select("#datetime");
 var inputFieldCity = d3.select("#city");
+var inputFieldState = d3.select("#state");
+var iinputFieldCountry = d3.select("#country");
+var iinputFieldDuration = d3.select("#duration");
+
+
+
 var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
 
@@ -46,16 +52,14 @@ button.on("click", () => {
   // console.log(filterCity) works
 
 
+// state
+  var inputState = inputFieldState.property("value").toLowerCase().trim();
+  console.log(inputState)
+
+  var filterState = dataTable.filter(dataTable => dataTable.state === inputState);
+console.log(filterState) 
+
 //////////////////////////////////////so far the above work do not touch
-
-
-//state
-  // var inputState = inputFieldState.property("string").toLowerCase().trim();
-  // // console.log(inputState)
-
-  // var filterState = dataTable.filter(dataTable => dataTable.state === inputState);
-// console.log(filterState) does NOT work
-
 
   // ////country
   // var inputCountry = inputFieldCountry.property("value").toLowerCase().trim();
@@ -69,26 +73,30 @@ button.on("click", () => {
   // var inputdurationMinutes = inputFieldDuration.property("value").(dataTable => dataTable.durationMinutes === inputdurationMinutes);
   // // console.log(inputDuration)
 
-  // var filterdurationMinutes =S dataTable.filter(dataTable => dataTable.durationMinutes === inputdurationMinutes);
+  // var filterdurationMinutes =dataTable.filter(dataTable => dataTable.durationMinutes === inputdurationMinutes);
   // // console.log(filterDuration) // // console.log(filterDuration) does NOT work
 
 
 
+
 ////////////////////////////////////////////////////////////////////////////////////// below works 
-  var filterCombinedData = dataTable.filter(dataTable => dataTable.datetime === inputDate && dataTable.city === inputCity);
-  // console.log(filterCombinedData) works
+  var filterCombinedData = dataTable.filter(dataTable => 
+    dataTable.datetime === inputDate &&
+    dataTable.city === inputCity &&
+    dataTable.state === inputState); //within the ()
+  // console.log(filterCombinedData) works add other tables here
  
 
-  //Filter data and clean 
+  //Filter data and clean  - add here too
   $tbody.html("");
   let response = {
-      filterDate, filterCity,  filterCombinedData
+      filterDate, filterCity,  filterCombinedData, filterState,
   }
   if(response.filterCombinedData.length !== 0) {
     addData(filterCombinedData);
 }
-else if(response.filterCombinedData.length === 0 && ((response.filterDate.length !== 0 || response.filterCity.length !== 0))) {
-  addData(filterDate) || addData(filterCity);
+else if(response.filterCombinedData.length === 0 && ((response.filterDate.length !== 0 || response.filterCity.length !== 0 || response.filterState.length !== 0))) {
+  addData(filterDate) || addData(filterCity) || addData (filterState) ;
 }
 
     // add comment if not sightings
