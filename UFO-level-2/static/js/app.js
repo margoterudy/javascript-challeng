af84b3fed397fa96b185ab0e9d19664717d26b3a
Margot <margoterudy@gmail.com>
2021-12-09 18:20:35 -0600

    fix image

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,25 +1,18 @@
-// from data.js USED CODE FROM LEVEL ONE 
+// from data.js
 var dataTable = data;
-
-// Viewing the available data frp, JS
-console.log(dataTable);
-
-// Create References
-var tbody = d3.select("tbody");
 
 // TEST 
 // tbody.append("tr").append("td").text("Hello!");
 
-//FORMAT AND FILTER BUTTON
+// //FORMAT AND FILTER BUTTON (code from level 1)
 var $tbody = d3.select("tbody");
 var button = d3.select("#filter-btn");
 var inputFieldDate = d3.select("#datetime");
 var inputFieldCity = d3.select("#city");
-var columns = ["datetime", "city", "state", "country", "shape", "comments"]
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
 
-
-//Insert Data into HTML
+// Input the data into the HTML
 var addData = (dataInput) => {
     dataInput.forEach(ufoSightings => {
         var row = $tbody.append("tr");
@@ -28,53 +21,43 @@ var addData = (dataInput) => {
     });
 }
 
-//add Data table
+//Add dataTable
 addData(dataTable);
 
-//Create event listeners (from 3rd lesson of JS)
-//We can use the `on` function in d3 to attach an event to the handler function
-// button.on("click",() => {
 
-//         d3.event.preventDefault();
+//Create eventListener for button
+button.on("click", () => {
 
-//         var inputDate = inputFieldDate.property("value").trim();
-//         // console.log(inputDate)
+  d3.event.preventDefault();
+  var inputDate = inputFieldDate.property("value").trim();
+  // console.log(inputDate)
 
-            // test
-    // console.log(inputDate)
+  var inputCity = inputFieldCity.property("value").toLowerCase().trim();
+  // console.log(inputCity)
 
-        // var inputCity = inputFieldCity.property("value").toLowerCase().trim();
-        // // console.log(inputCity)
+  var filterDate = dataTable.filter(dataTable => dataTable.datetime === inputDate);
+  // console.log(filterDate)
 
-        // var filterDate = dataTable.filter(dataTable => dataTable.datetime === inputDate);
-        // // console.log(filterDate)
+  var filterCity = dataTable.filter(dataTable => dataTable.city === inputCity);
+  // console.log(filterCity)
 
-        // var filterCity = dataTable.filter(dataTable => dataTable.city === inputCity);
-        // // console.log(filterCity)
+  var filterCombinedData = dataTable.filter(dataTable => dataTable.datetime === inputDate && dataTable.city === inputCity);
+  // console.log(filterCombinedData)
 
-        // var filterCombinedData = dataTable.filter(dataTable => dataTable.datetime === inputDate && dataTable.city === inputCity);
-        // // console.log(filterCombinedData)
-
-  // //Filter data and clean 
-  // var filterDate = dataTable.filter(dataTable => dataTable.datetime === inputDate);
-  // $tbody.html("");
-
-
-//   let response = {
-//     filterDate, filterCity, filterCombinedData
-// }
-// if(response.filterCombinedData.length !== 0) {
-//   addData(filterCombinedData);
-// }
-// else if(response.filterCombinedData.length === 0 && ((response.filterDate.length !== 0 || response.filterCity.length !== 0))) {
-// addData(filterDate) || addData(filterCity);
-// }
-
-  // add comment if not sightings
-//       else {
-//           $tbody.append("tr").append("td").text("There is NOTHING here! I SAY, GOOD DAY SIR");
-//       }
-// })
+  $tbody.html("");
+    let response = {
+        filterDate, filterCity, filterCombinedData
+    }
+    if(response.filterCombinedData.length !== 0) {
+      addData(filterCombinedData);
+  }
+  else if(response.filterCombinedData.length === 0 && ((response.filterDate.length !== 0 || response.filterCity.length !== 0))) {
+    addData(filterDate) || addData(filterCity);
+}
+else {
+  $tbody.append("tr").append("td").text("There is nothing here! I say GOOD DAY SIR!!!");
+}
+})
 
 
 
